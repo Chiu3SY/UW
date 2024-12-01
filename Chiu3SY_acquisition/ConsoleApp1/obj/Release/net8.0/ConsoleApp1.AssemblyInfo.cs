9aa06fa36bc9b13be5b8fb4ace8d1746682654c0
Songyi <1837356130@qq.com>
2024-12-01 15:14:13 +0800

    second_commit_test_2024_12_1

--- Chiu3SY_acquisition/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Chiu3SY_acquisition/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d52d0cd781718da210d740990d6c4aef1f281c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
